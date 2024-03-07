49979b1b471aa89e85d043425befd69babdd4fe8
kirtis20 <kirtis2005@gmail.com>
2024-03-07 21:26:10 +0530

    app_bar changed

--- lib/utils/app_bar.dart
+++ lib/utils/app_bar.dart
@@ -1,3 +1,4 @@
+
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_field
 
 import 'package:cook_up/pages/ingrident_search.dart';
@@ -6,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
 class Appbar extends StatefulWidget {
-  const Appbar({super.key});
+  const Appbar({Key? key}) : super(key: key);
 
   @override
   State<Appbar> createState() => _AppbarState();
@@ -14,9 +15,7 @@ class Appbar extends StatefulWidget {
 
 class _AppbarState extends State<Appbar> {
   int _indexSelected = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
-  static const List<Widget> _widget = <Widget>[
+  static const List<Widget> _widgetOptions = <Widget>[
     SearchPage(),
     SearchIngridients(),
   ];
@@ -24,34 +23,40 @@ class _AppbarState extends State<Appbar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _widgetOptions.elementAt(_indexSelected),
       bottomNavigationBar: GNav(
-          rippleColor: Color.fromARGB(255, 192, 155, 34),
-          hoverColor: Color.fromARGB(255, 205, 192, 11),
-          gap: 6,
-          activeColor: Colors.black,
-          iconSize: 24,
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-          duration: Duration(milliseconds: 400),
-          tabBackgroundColor: Color.fromARGB(255, 183, 98, 25),
-          color: Color.fromARGB(255, 188, 147, 33),
-          tabs: [
-            GButton(
-              icon: Icons.search,
-              text: 'Search',
-              haptic: true,
-            ),
-            GButton(
-              icon: Icons.book,
-              text: 'Recipes',
-              haptic: true,
-            ),
-          ],
-          selectedIndex: _indexSelected,
-          onTabChange: (index) {
-            setState(() {
-              _indexSelected = index;
-            });
-          }),
+        rippleColor: Color.fromARGB(255, 192, 155, 34),
+        hoverColor: Color.fromARGB(255, 205, 192, 11),
+        gap: 6,
+        activeColor: Colors.black,
+        iconSize: 24,
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+        duration: Duration(milliseconds: 400),
+        tabBackgroundColor: Color.fromARGB(255, 183, 98, 25),
+        color: Color.fromARGB(255, 188, 147, 33),
+        tabs: [
+          GButton(
+            icon: Icons.search,
+            text: 'Search',
+            onPressed: () {
+              setState(() {
+                _indexSelected = 0;
+              });
+            },
+          ),
+          GButton(
+            icon: Icons.book,
+            text: 'Recipes',
+            onPressed: () {
+              setState(() {
+                _indexSelected = 1;
+              });
+            },
+          ),
+        ],
+      ),
     );
   }
 }
+
+
