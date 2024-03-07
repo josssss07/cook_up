92a12bb2a6d09c016ec0161c5a87da6437677d7d
josssss07 <joshuadesai03@gmail.com>
2024-03-07 23:29:17 +0530

    api refactor

---
+++ lib/fetch_api_data/fetch_ing.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+
+import '../api_concept_test/data/fetch_recipie.dart';
+import 'package:http/http.dart' as http;
+
+class SpoonacularData {
+  final String apiKey;
+  SpoonacularData({required this.apiKey});
+
+  Future<List<Future<RecipeWithDetails>>> fetchRecipes(
+      String apiKey, String ingridients) async {
+    final url = Uri.parse(
+        "https://api.spoonacular.com/recipes/search?apiKey=$apiKey&includeIngredients=$ingridients");
+
+    final response = await http.get(url);
+
+    if(response.statusCode == 200){
+      final data = jsonDecode(response.body)["results"] as List;
+      return data.map((recipeJson) => _fetchRecipeDetails(recipeJson)).toList();
+    } else {
+      throw Exception("Failed to fetch recipes");
+    }
+    
+  }
+
+  Future<RecipeWithDetails> _fetchRecipeDetails(Map<String, dynamic> recipeJson) async {
+    final recipeId = recipeJson["id"] as int;
+    final url = Uri.parse(
+        "https://api.spoonacular.com/recipes/$recipeId/information?apiKey=$apiKey&includeInstructions=true");
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final details = jsonDecode(response.body);
+      return RecipeWithDetails(
+        id: recipeJson["id"] as int,
+        title: recipeJson["title"] as String,
+        image: recipeJson["image"] as String,
+        ingredients: (recipeJson["missedIngredients"] as List)
+            .map((ingredient) => ingredient["name"] as String)
+            .toList(),
+        instructions: details["instructions"] as String,
+      );
+    } else {
+      throw Exception("Failed to fetch recipe details");
+    }
+  }
+}
