650ce1a08e39151b06f6134641c9205aaff4b73c
josssss07 <joshuadesai03@gmail.com>
2024-03-07 19:56:12 +0530

    ignores added

--- lib/utils/app_bar.dart
+++ lib/utils/app_bar.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_field
 
 import 'package:cook_up/pages/ingrident_search.dart';
 import 'package:cook_up/pages/search_page.dart';
--- lib/widgets/user_info_tile.dart
+++ lib/widgets/user_info_tile.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_typing_uninitialized_variables, unused_local_variable
+
 import 'package:flutter/material.dart';
 import 'package:cook_up/utils/AppColor.dart';
 
